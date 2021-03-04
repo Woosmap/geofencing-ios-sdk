e0ed1c3c8f131c84c2e5d6152b55d3cf3d265f37
Llumbroso <51318920+Llumbroso@users.noreply.github.com>
2021-03-04 14:02:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WoosmapGeofencing",
-            dependencies: ["Surge","Realm"],
+            dependencies: ["Surge","RealmSwift"],
             path: "WoosmapGeofencing/Sources/WoosmapGeofencing"),
         .testTarget(
             name: "WoosmapGeofencingTests",
