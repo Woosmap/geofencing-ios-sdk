4e83d7bc8bc56d687ce7493a152787b9f2d9ada0
llumbroso <llumbroso@webgeoservices.com>
2021-03-11 14:18:12 +0100

    event in lower case

--- Sample/Data/AirshipEvents.swift
+++ Sample/Data/AirshipEvents.swift
@@ -17,7 +17,7 @@ public class AirshipEvents: AirshipEventsDelegate {
     
     public func regionEnterEvent(regionEvent: Dictionary<String, Any>) {
         #if canImport(AirshipCore)
-            let event = UACustomEvent(name: "Geofence_entered_event", value: 1)
+            let event = UACustomEvent(name: "geofence_entered_event", value: 1)
             event.properties = regionEvent
             event.track()
         #endif
@@ -25,7 +25,7 @@ public class AirshipEvents: AirshipEventsDelegate {
     
     public func regionExitEvent(regionEvent: Dictionary<String, Any>) {
         #if canImport(AirshipCore)
-            let event = UACustomEvent(name: "Geofence_exited_event", value: 1)
+            let event = UACustomEvent(name: "geofence_exited_event", value: 1)
             event.properties = regionEvent
             event.track()
         #endif
@@ -33,7 +33,7 @@ public class AirshipEvents: AirshipEventsDelegate {
     
     public func visitEvent(visitEvent: Dictionary<String, Any>) {
         #if canImport(AirshipCore)
-            let event = UACustomEvent(name: "Visit_event", value: 1)
+            let event = UACustomEvent(name: "visit_event", value: 1)
             event.properties = visitEvent
             event.track()
         #endif
@@ -41,7 +41,7 @@ public class AirshipEvents: AirshipEventsDelegate {
     
     public func poiEvent(POIEvent: Dictionary<String, Any>) {
         #if canImport(AirshipCore)
-            let event = UACustomEvent(name: "POI_event", value: 1)
+            let event = UACustomEvent(name: "poi_event", value: 1)
             event.properties = POIEvent
             event.track()
         #endif
